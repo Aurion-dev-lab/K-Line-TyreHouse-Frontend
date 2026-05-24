b0141b6280c1d1ba803aa8c609d0994fcf33cd42
Minoka Wickramasinghe <76654300+Minokainduwara@users.noreply.github.com>
2026-05-24 21:04:48 +0530

    Feat: Alerts

--- src/main/java/com/gui/kline/utils/AlertUtil.java
+++ src/main/java/com/gui/kline/utils/AlertUtil.java
@@ -1,4 +1,21 @@
 package com.gui.kline.utils;
 
+import javafx.scene.control.Alert;
+
 public class AlertUtil {
+    public static void showInfo(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+    public static void showError(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
 }
