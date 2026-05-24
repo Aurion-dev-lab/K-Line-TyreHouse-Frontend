0554d0db19f369ab37935f4c9659ef9b0abd62f8
Minoka Wickramasinghe <76654300+Minokainduwara@users.noreply.github.com>
2026-05-24 16:58:04 +0530

    Feat: Connect The Database

--- src/main/java/com/gui/kline/App.java
+++ src/main/java/com/gui/kline/App.java
@@ -1,7 +1,10 @@
 package com.gui.kline;
 
+import com.gui.kline.data.DatabaseManager;
 import com.gui.kline.models.ViewModel;
+import com.gui.kline.utils.AlertUtil;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -9,6 +12,13 @@ import java.io.IOException;
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        try {
+            DatabaseManager.init();
+        } catch (RuntimeException ex) {
+            AlertUtil.showError("Database error", ex.getMessage());
+            Platform.exit();
+            return;
+        }
         ViewModel.INSTANCE.getViewsFactory().getView("main-layout");
     }
 
